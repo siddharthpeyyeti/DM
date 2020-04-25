1c4c7d1ea5a6e60a150885e11d4bf9063ea1f4c2
siddharthpeyyeti <39346024+siddharthpeyyeti@users.noreply.github.com>
2020-04-25 18:39:52 +0530

    Add files via upload
    Linear regression on annual rainfall/year for all 36 divisions
    Linear regression on TN annual rainfall/year 20 years at at time

---
+++ LinearRegression_TN_20yrs_80-20.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 25 17:03:33 2020
+
+@author: Siddharth
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 21:42:35 2020
+
+@author: Siddharth
+"""
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from math import sqrt
+#import os
+#x=os.getcwd()
+rf_data=pd.read_csv(r'C:\Users\Siddharth\Desktop\BITS\3 2\DM\project\Sub_Division_IMD_2017.csv')
+
+###Calculate Mean of a column
+def cal_mean(readings):
+
+    readings_total = sum(readings)
+    number_of_readings = len(readings)
+    mean = readings_total / float(number_of_readings)
+    return mean
+ 
+###Calculate variance of a column
+def cal_variance(readings):
+    # To calculate the variance we need the mean value
+    # Calculating the mean value from the cal_mean function
+    readings_mean = cal_mean(readings)
+    # mean difference squared readings
+    mean_difference_squared_readings = [pow((reading - readings_mean), 2) for reading in readings]
+    variance = sum(mean_difference_squared_readings)
+    return variance / float(len(readings) - 1)
+ 
+###Calculate Covariance of a column
+def cal_covariance(readings_1, readings_2):
+
+    readings_1_mean = cal_mean(readings_1)
+    readings_2_mean = cal_mean(readings_2)
+    readings_size = len(readings_1)
+    covariance = 0.0
+    for i in range(0, readings_size):
+        covariance += (readings_1[i] - readings_1_mean) * (readings_2[i] - readings_2_mean)
+    return covariance / float(readings_size - 1)
+ 
+###Calculate the linear regression coefficients
+
+def lin_reg(df):
+    # Calculating the mean of the years and the annual rainfall
+    years_mean = cal_mean(df['YEAR'])
+    rainfall_mean = cal_mean(df['value'])
+    
+    # Calculating the variance of the years and the annual rainfall
+    years_variance = cal_variance(df['YEAR'])
+    
+    # Calculating the regression    
+    covariance_of_rainfall_and_years = df.cov()['YEAR']['value']
+
+    w1 = covariance_of_rainfall_and_years / float(years_variance)
+     
+    w0 = rainfall_mean - (w1 * years_mean)
+    
+    return [w0, w1]
+
+ ###Calculate rmse for given dataframe
+def cal_rmse(df):
+    soe = 0
+    n = len(df['value'])
+    for x in range (n):
+        actual = test_vals['value'].iloc[x]
+        predicted = test_vals['Predicted_Rainfall'].iloc[x]
+        soe = soe + ((actual-predicted)**2)/n
+    return sqrt(soe)
+
+
+###Load the data
+
+x = rf_data.SUBDIVISION
+states=[]
+
+for i in range(len(x)):
+    if x[i] not in states:
+        states.append(x[i])
+
+###list of state dataframes
+l_state_dfs = []
+
+for i in range(len(states)):
+    n=states[i]
+    x=rf_data[rf_data.SUBDIVISION == n]
+    l_state_dfs.append(x)
+
+###create list of columns to drop
+months = ['SUBDIVISION', 'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC', 'JF', 'MAM', 'JJAS', 'OND']
+
+###drop the columns for all dataframes and melt
+for i in range(len(states)):
+    l_state_dfs[i]=l_state_dfs[i].drop(months, axis=1)
+    l_state_dfs[i] = l_state_dfs[i].melt('YEAR').reset_index()
+
+###Remove nans (loop through states and replace with annual mean)
+for i in range(len(states)):
+    mean_of_state=l_state_dfs[i].value.mean()
+    l_state_dfs[i].value = l_state_dfs[i].value.fillna(mean_of_state)
+
+'''
+###normalize annual rainfall
+na=[]
+for i in range(len(states)):
+    a=min(l_state_dfs[i].value)
+    b=max(l_state_dfs[i].value)
+    
+    for j in range(len(l_state_dfs[i].value)):
+        u = (l_state_dfs[i]['value'][j]-a)/(b -a)
+        na.append(u)
+    
+    l_state_dfs[i].drop(['value'],axis=1)
+    l_state_dfs[i]['value'] = na
+    na = []
+'''
+    
+###Using functions defined to perform linear regression
+    
+errors = [] #list of RMSE for each state
+
+###10 YEARS AT A TIME FOR TAMIL NADU
+for i in range(len(states)):
+    if (states[i]=='Tamil Nadu'):
+        break
+
+###get 10 values at a time
+while (not l_state_dfs[i].empty):
+    #first 10 rows of df
+    ten_rows = l_state_dfs[i].head(20)
+    full=ten_rows
+    #take 1st 7/10 rows
+    ten_rows=ten_rows.head(int(len(ten_rows)*(80/100)))
+    #do linear regression on seven rows
+    coeffs = lin_reg(ten_rows)
+    w0 = coeffs[0]
+    w1 = coeffs[1]
+    #predict for ten rows
+    full['Predicted_Rainfall'] = w0 + w1 * full['YEAR']
+    #RMSE calculation for last 3 rows
+    test_vals = full.tail(int(len(l_state_dfs[i])*(20/100))+1)
+    rmse = cal_rmse(test_vals)
+    errors.append(rmse)
+    #remove the 10 rows from the dataframe and perform the same operations on next ten rows
+    l_state_dfs[i] = l_state_dfs[i].iloc[20:]
+
+    
+    ###plot
+    plt.figure()
+    plt.plot(full['YEAR'], full['value'])
+    plt.plot(full['YEAR'], full['Predicted_Rainfall'])
+    plt.title(states[i])
+    plt.xlabel('YEAR')
+    plt.ylabel('Annual Rainfall in mm')
+    
+    '''
+    ###convert annual rainfall back to mm
+    na=[]
+    for j in range(len(l_state_dfs[i].value)):
+        u=l_state_dfs[i]['Predicted_Rainfall'][j]*(b -a)+a 
+        na.append(u)
+    l_state_dfs[i].drop(['Predicted_Rainfall'],axis=1)
+    l_state_dfs[i]['Predicted_Rainfall'] = na
+    '''
